cb447547eed7000d2181a9ffc67448531a2522fd
Andrei Vladescu <52881237+AndreiVladescu@users.noreply.github.com>
2025-01-08 08:09:28 +0200

    Changed CRLF to LF

--- server.py
+++ server.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def handle_get():
-    # HTTP GET request response
     response_content = "ls"
     return Response(response_content, status=200, mimetype='text/plain')
 
 if __name__ == "__main__":
-    # Start the Flask server on localhost:8080
     app.run(host='127.0.0.1', port=8080)
